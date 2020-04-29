a26eb6ff789e43c56ff56bb82429602c22efc4ed
Chris Houghton <chris@beaconcrm.org>
2020-04-29 21:38:41 +0100

    tidy ups

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const wait = require('./wait');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
     const ms = core.getInput('milliseconds');
     console.log(`Waiting ${ms} milliseconds ...`);
     console.log('env variables are below:');
     console.log(process.env);
 
-    core.debug((new Date()).toTimeString())
+    core.debug((new Date()).toTimeString());
     await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    core.debug((new Date()).toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
